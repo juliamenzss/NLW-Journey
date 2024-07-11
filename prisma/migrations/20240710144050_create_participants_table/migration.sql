85a21f26b2fa5c45d1eee6d2b29d9b03cce360ed
juliamenzss <juliamenzss@gmail.com>
2024-07-11 10:52:06 -0300

    first commit

---
+++ prisma/migrations/20240710144050_create_participants_table/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Trip` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Trip";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "trips" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "destination" TEXT NOT NULL,
+    "starts_at" DATETIME NOT NULL,
+    "ends_at" DATETIME NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "participants" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "is_owner" BOOLEAN NOT NULL DEFAULT false,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "participants_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "trips" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
